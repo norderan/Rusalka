feab21c69ce1d7e56cc305be9e3754a56e2fb171
Yazan <queider@proton.me>
2024-10-20 23:54:59 +0300

    Made last changes on graph

--- js.js
+++ js.js
@@ -2,9 +2,8 @@ const stockCanvas = document.getElementById("stockCanvas");
 const ctx = stockCanvas.getContext("2d");
 
 
-let trend = 0.69;
+let trend = 0.8;
 let volatility = 0.05;
-console.log(createStockGraph(trend, volatility));
 drawStock(createStockGraph(trend, volatility));
 
 
@@ -16,15 +15,15 @@ drawStock(createStockGraph(trend, volatility));
 
 
 function createStockGraph (trend, volatility) {
-    let tips = Math.floor(Math.random() * 50) + 100;
+    let tips = 100;
     const coordinatesArray = [];
     let firstYcoordinates = Math.floor(Math.random() * 100) + 200;
     let firstCoordinates  = {x: 0, y:firstYcoordinates}
     coordinatesArray.push(firstCoordinates);
 
-    const tipsGap = Math.round(1000 / (tips + 1 ));
+    const tipsGap = Math.round(1000 / (tips + 1));
     let tipsGapCounter = tipsGap;
-    for (let i = 1; i < tips+ 1; i++){
+    for (let i = 1; i < tips + 1; i++){
         let priceChange;
         if(Math.random() < trend){
             priceChange = (Math.random()* (volatility)) + 1 - volatility; 
@@ -35,10 +34,8 @@ function createStockGraph (trend, volatility) {
         let coordinates = {x: tipsGapCounter, y:Math.floor(lastPrice * priceChange)}
         tipsGapCounter = tipsGapCounter + tipsGap;
         coordinatesArray.push(coordinates);
-        if(i + 1 === tips + 1) {
-        coordinatesArray.push({x: 1000, y: lastPrice * priceChange});
-        }
     }
+    console.log(coordinatesArray);
     return coordinatesArray;
 }
 
@@ -48,10 +45,17 @@ function drawStock(coordinatesArray) {
     const startingCoordinates = coordinatesArray[0];
     ctx.moveTo(startingCoordinates.x, startingCoordinates.y);
 
+    const delay = 30;  // 100 ms = 0.1 seconds
+
     for (let i = 1; i < coordinatesArray.length; i++) {
-        const coordinates = coordinatesArray[i];
-        ctx.lineTo(coordinates.x, coordinates.y);
+        // Use a closure to preserve the current value of i and delay the drawing of each line
+        setTimeout(function() {
+            const coordinates = coordinatesArray[i];
+            ctx.lineTo(coordinates.x, coordinates.y);
+            ctx.lineWidth = 5;
+            ctx.stroke();
+        }, delay * i); // Multiply delay by 'i' to create increasing delay between lines
     }
-    ctx.lineWidth = 5;
-    ctx.stroke();
 }
+
+
