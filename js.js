50205d8a8cd463ecf1bc5e7257ca6ca6f00996f0
Yazan <queider@proton.me>
2024-10-25 18:02:14 +0300

    Add basic game in console for one graph

--- js.js
+++ js.js
@@ -1,24 +1,155 @@
-const stockCanvas = document.getElementById("stockCanvas");
+let dayCounter = 0;
+const dayCounterElement = document.getElementById("dayCounter");
+const stockCanvas = document.getElementById("canvas1");
 const ctx = stockCanvas.getContext("2d");
 
+let news;
+let priceCanvas1 = 1000;
+let userCredits = 1000;
+let command;
 
-let trend = 0.8;
-let volatility = 0.05;
-drawStock(createStockGraph(trend, volatility));
+const PRECENTAGE_DIVIDOR_FACTOR = 0.2;
 
 
+
+/*
+
+
+
+*/
+dayLoop();
+
+
+function dayLoop(){
+    console.log("==================================")
+    console.log("day: " + dayCounter);
+    console.log("user credits: " + userCredits);
+    console.log("canvas price: " + priceCanvas1);
+    ctx.clearRect(0, 0, 400, 200);
+    //here is the day loop
+    dayCounterElement.textContent = "Day " + (dayCounter + 1);
+    news = "mig32 test fail above kazakhi lands";
+    const newsHeadline = document.getElementById("newsHeadline");
+    newsHeadline.textContent = news;
+    stockArray1 = createStockArray(0.7, 0.05);
+    drawStock(stockArray1, true);
+
+}
+
+
+
+
+//terminal:
+let terminalInput;
+const commandHistory = document.getElementById("commandHistory");
+const terminalInputElement = document.getElementById("terminalInput");
+terminalInputElement.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+        terminalInput = terminalInputElement.value;
+        terminalInterpreter(terminalInput);
+        console.log('terminal command: '+ terminalInput);
+        let newCommand = document.createElement("p");
+        newCommand.textContent = terminalInput;
+        newCommand.classList.add("command");
+        commandHistory.appendChild(newCommand);
+        terminalInputElement.value = '';
+        commandHistory.scrollTop = commandHistory.scrollHeight;
+    }
+});
+
+// Interpretes terminal commands
+function terminalInterpreter (commandString) {
+    let commandArray = commandString.split(" ");
+    if(commandArray[0] == "trade"){
+        let isLong;
+        if (commandArray[1] == "long") {
+            isLong = true;
+        } else {
+            isLong = false;
+        }
+        profit = trade(stockArray1, isLong, commandArray[2]);
+        userCredits = userCredits + profit;
+
+        drawStock(stockArray1, false);
+        priceCanvas1 = calculateNewPrice(stockArray1, priceCanvas1)
+        console.log("user credits: " + userCredits);
+        console.log("canvas price: " + priceCanvas1);
+        console.log("==================================")
+
+    }
+    if(commandArray[0] == "c") {
+        dayCounter++;
+        dayLoop();
+    }
+}
+
+
+// calculates the trade profits.
+function trade(stockArray, long, amount, takeProfitPixels, stopLossPixels) {
+    let profit;
+    let percentage
+    userCredits = userCredits - amount; 
+    if (takeProfitPixels === undefined && stopLossPixels === undefined) {
+        if (long){
+
+            percentage = (stockArray[25].y / stockArray[100].y) * PRECENTAGE_DIVIDOR_FACTOR;
+            profit =  percentage * amount;
+        } else {
+            percentage = (stockArray[100].y/ stockArray[25].y) * PRECENTAGE_DIVIDOR_FACTOR;
+            profit =  percentage * amount;
+        }
+    }else if(takeProfitPixels === undefined){
+        for(let i = 26; i < 101; i++){
+            if(stockArray[i].y >= stopLossPixels) {
+
+                if (long){
+                    profit = (stockArray[25].y / stockArray[i].y) * amount;
+                } else {
+                    profit = (stockArray[i].y/ stockArray[25].y) * amount;
+                }
+                break;
+            }
+        }
+        return profit;
+    } else if (stopLossPixels === undefined){
+        for(let i = 26; i < 101; i++){
+            if(stockArray[i].y <= takeProfitPixels ) {
+                if (long){
+                    profit = (stockArray[25].y / stockArray[i].y) * amount;
+                } else {
+                    profit = (stockArray[i].y/ stockArray[25].y) * amount;
+                }
+            }
+        } 
+    } else {
+        for(let i = 26; i < 101; i++){
+            if(stockArray[i].y <= takeProfitPixels || stockArray[i].y >= stopLossPixels) {
+                if (long){
+                    profit = (stockArray[25].y / stockArray[i].y) * amount;
+                } else {
+                    profit = (stockArray[i].y/ stockArray[25].y) * amount;
+                }
+            }
+        } 
+    }
+    console.log("trade for day " + dayCounter + ":");
+    console.log("amount: " + amount);
+    console.log("profit percentage: " + percentage);
+    console.log("net profit: " + Math.round(profit));
+    return Math.round(profit); 
+}
 
 //This funcation returns an array of stock coordinates.
-//volatility - How "hard" the graph goes up and down. 1 --- 10.
-//trend - is and how many does the graph goes up or down. 0 - 1
-function createStockGraph (trend, volatility) {
+//volatility - How "hard" the graph goes up and down. 0 - 1
+//trend - trend up or down. 0 - 1
+function createStockArray (trend, volatility) {
     let tips = 100;
     const coordinatesArray = [];
-    let firstYcoordinates = Math.floor(Math.random() * 100) + 200;
+    let firstYcoordinates = 100;
     let firstCoordinates  = {x: 0, y:firstYcoordinates}
     coordinatesArray.push(firstCoordinates);
 
-    const tipsGap = Math.round(1000 / (tips + 1));
+    const tipsGap = Math.round(400 / (tips + 1));
     let tipsGapCounter = tipsGap;
     for (let i = 1; i < tips + 1; i++){
         let priceChange;
@@ -28,23 +159,39 @@ function createStockGraph (trend, volatility) {
             priceChange = (Math.random()* (volatility))+ 1 + volatility;
         }
         let lastPrice = coordinatesArray[i - 1].y;
-        let coordinates = {x: tipsGapCounter, y:Math.floor(lastPrice * priceChange)}
+        let coordinates = {x: tipsGapCounter, y:lastPrice * priceChange}
         tipsGapCounter = tipsGapCounter + tipsGap;
         coordinatesArray.push(coordinates);
     }
-    console.log(coordinatesArray);
     return coordinatesArray;
 }
 
+// calculates a price from the pixel change
+function calculateNewPrice(stockArray, lastPrice) {
+    return Math.round((lastPrice * stockArray[0].y )  / stockArray[stockArray.length - 1].y)
+}
 
-function drawStock(coordinatesArray) {
-    ctx.beginPath();  
-    const startingCoordinates = coordinatesArray[0];
+
+// Draws the stock price on a canvas
+function drawStock(coordinatesArray, isHalf) {
+    let startingCoordinates;
+    let endingCoordinatesIndex;
+    let startingCoordinatesIndex;
+    if(isHalf) {
+        startingCoordinates = coordinatesArray[0];
+        endingCoordinatesIndex = 26;
+        startingCoordinatesIndex = 0;
+    } else {
+        startingCoordinates = coordinatesArray[25];
+        endingCoordinatesIndex = 101;
+        startingCoordinatesIndex = 25;
+    }
+
     ctx.moveTo(startingCoordinates.x, startingCoordinates.y);
+    ctx.beginPath();  
 
     const delay = 30;  // 100 ms = 0.1 seconds
-
-    for (let i = 1; i < coordinatesArray.length; i++) {
+    for (let i = startingCoordinatesIndex; i < endingCoordinatesIndex; i++) {
         // Use a closure to preserve the current value of i and delay the drawing of each line
         setTimeout(function() {
             const coordinates = coordinatesArray[i];
@@ -56,20 +203,3 @@ function drawStock(coordinatesArray) {
 }
 
 
-//terminal:
-
-let terminalInput;
-const commandHistory = document.getElementById("commandHistory");
-const terminalInputElement = document.getElementById("terminalInput");
-terminalInputElement.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-        terminalInput = terminalInputElement.value;
-        console.log('terminal command: '+ terminalInput);
-        let newCommand = document.createElement("p");
-        newCommand.textContent = terminalInput;
-        newCommand.classList.add("command");
-        commandHistory.appendChild(newCommand);
-        terminalInputElement.value = '';
-        commandHistory.scrollTop = commandHistory.scrollHeight;
-    }
-});
