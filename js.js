672ded02b39164269c34b703ac60124123f62c95
Yazan <queider@proton.me>
2024-11-05 19:54:07 +0200

    Add support for four graphs with no multi selectoin

--- js.js
+++ js.js
@@ -1,10 +1,22 @@
 let dayCounter = 0;
 const dayCounterElement = document.getElementById("dayCounter");
-const stockCanvas = document.getElementById("canvas1");
-const ctx = stockCanvas.getContext("2d");
+const stockCanvas1 = document.getElementById("canvas1");
+const stockCanvas2 = document.getElementById("canvas2");
+const stockCanvas3 = document.getElementById("canvas3");
+const stockCanvas4 = document.getElementById("canvas4");
+
+const ctxCanvas1 = stockCanvas1.getContext("2d");
+const ctxCanvas2 = stockCanvas2.getContext("2d");
+const ctxCanvas3 = stockCanvas3.getContext("2d");
+const ctxCanvas4 = stockCanvas4.getContext("2d");
+
 
 let news;
 let priceCanvas1 = 1000;
+let priceCanvas2 = 700;
+let priceCanvas3 = 800;
+let priceCanvas4 = 500;
+
 let userCredits = 1000;
 let command;
 
@@ -15,9 +27,15 @@ const CANVAS_HEIGHT = 200;
 const CANVAS_WIDTH = 400;
 
 
-let stockArray1;
-let stockArrayCoords;
-let stockArray1draw;
+let stockArrayCoords1;
+let stockArrayCoords2;
+let stockArrayCoords3;
+let stockArrayCoords4;
+
+let stockArrayDraw1;
+let stockArrayDraw2;
+let stockArrayDraw3;
+let stockArrayDraw4;
 /*
 Tasks: 
     - create a news funciton
@@ -35,19 +53,32 @@ function dayLoop() {
     console.log("day: " + dayCounter);
     console.log("user credits: " + userCredits);
     console.log("canvas price: " + priceCanvas1);
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctxCanvas1.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctxCanvas2.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctxCanvas3.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctxCanvas4.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
     //here is the day loop
     dayCounterElement.textContent = "Day " + (dayCounter + 1);
     news = "mig32 test fail above kazakhi lands";
     const newsHeadline = document.getElementById("newsHeadline");
     newsHeadline.textContent = news;
-    stockArrayCoords = createStockArray(0.4, 0.004);
 
-    stockArray1draw = stockArrayCoords.map(element => CANVAS_HEIGHT - Number(((element * 1000) - 900).toFixed(1)));;
-    stockArray1 = stockArrayCoords.map(element => Number(((element * 1000) - 900).toFixed(1)));;
+    stockArrayCoords1 = createStockArray(0.4, 0.004);
+    stockArrayCoords2 = createStockArray(0.4, 0.004);
+    stockArrayCoords3 = createStockArray(0.4, 0.004);
+    stockArrayCoords4 = createStockArray(0.4, 0.004);
+    
+    stockArrayDraw1 = stockArrayCoords1.map(element => CANVAS_HEIGHT - Number(((element * 1000) - 900).toFixed(1)));;
+    stockArrayDraw2 = stockArrayCoords2.map(element => CANVAS_HEIGHT - Number(((element * 1000) - 900).toFixed(1)));;
+    stockArrayDraw3 = stockArrayCoords3.map(element => CANVAS_HEIGHT - Number(((element * 1000) - 900).toFixed(1)));;
+    stockArrayDraw4 = stockArrayCoords4.map(element => CANVAS_HEIGHT - Number(((element * 1000) - 900).toFixed(1)));;
 
-    drawThe500lines(stockArray1draw, priceCanvas1);
-    drawStock(stockArray1draw, true); 
+    //drawThe500lines(stockArray1draw, priceCanvas1);
+    drawStock(stockArrayDraw1, true, ctxCanvas1);
+    drawStock(stockArrayDraw2, true, ctxCanvas2); 
+    drawStock(stockArrayDraw3, true, ctxCanvas3); 
+    drawStock(stockArrayDraw4, true, ctxCanvas4); 
 
 }
 
@@ -81,6 +112,9 @@ function addMessageToTerminal(message) {
 function terminalInterpreter(commandString) {
     let commandArray = commandString.split(" ");
     if (commandArray[0] == "trade") {
+        let priceCanvas;
+        let ctxCanvas;
+        let symbol = commandArray[1];
         let longOrshort = commandArray[2];
         let amount = commandArray[3];
         let isLong;
@@ -88,13 +122,15 @@ function terminalInterpreter(commandString) {
         let takeProfit;
         let stopLossPixels;
         let takeProfitPixels;
-
-        if (!/^[0-9]+$/.test(amount)) {
-            addMessageToTerminal("[-] Amount not valid.")
+        let stockArrayCoords;
+        if (symbol.toLowerCase() !== "lk" && symbol.toLowerCase() !== "gd" && symbol.toLowerCase() !== "su" && symbol.toLowerCase() !== "imp") {
+            addMessageToTerminal("[-] The trade symbol " + symbol + " is not valid.");
+        } else if (!/^[0-9]+$/.test(amount)) {
+            addMessageToTerminal("[-] Amount not valid.");
         } else if (amount > userCredits) {
-            addMessageToTerminal("[-] Not enough credits.")
+            addMessageToTerminal("[-] Not enough credits.");
         } else if (amount <= 0) {
-            addMessageToTerminal("[-] Amount not valid.")
+            addMessageToTerminal("[-] Amount not valid.");
         } else if (longOrshort != "long" && longOrshort != "short") {
             addMessageToTerminal("[-] Long or short not specified on a position");
         } else if (commandArray[5] && !/^[0-9]+$/.test(commandArray[5]) || commandArray[7] && !/^[0-9]+$/.test(commandArray[7])) {
@@ -117,32 +153,64 @@ function terminalInterpreter(commandString) {
             } else if (longOrshort == "short") {
                 isLong = false;
             }
+            
+            switch (symbol.toLowerCase()) {
+                case "lk":
+                    ctxCanvas = ctxCanvas1;
+                    priceCanvas = priceCanvas1;
+                    stockArrayCoords = stockArrayCoords1;
+                    break;
+                case "gd":
+                    ctxCanvas = ctxCanvas2;
+                    priceCanvas = priceCanvas2;
+                    stockArrayCoords = stockArrayCoords2;
+                    break;
+                case "su":
+                    ctxCanvas = ctxCanvas3;
+                    priceCanvas = priceCanvas3;
+                    stockArrayCoords = stockArrayCoords3;
+                    break;
+                case "imp":
+                    ctxCanvas = ctxCanvas4;
+                    priceCanvas = priceCanvas4;
+                    stockArrayCoords = stockArrayCoords4;
+                default:
 
+                    break;
+            }
             // draw stoploss line 
-            ctx.strokeStyle = "red";
-            ctx.lineWidth = 3;
-            ctx.beginPath();
-            ctx.moveTo(0, stopLossPixels);
-            ctx.lineTo(CANVAS_WIDTH, stopLossPixels);
-            ctx.stroke();
+            ctxCanvas.strokeStyle = "red";
+            ctxCanvas.lineWidth = 3;
+            ctxCanvas.beginPath();
+            ctxCanvas.moveTo(0, stopLossPixels);
+            ctxCanvas.lineTo(CANVAS_WIDTH, stopLossPixels);
+            ctxCanvas.stroke();
 
             // draw take profit line 
-            ctx.strokeStyle = "green";
-            ctx.lineWidth = 3;
-            ctx.beginPath();
-            ctx.moveTo(0, takeProfitPixels);
-            ctx.lineTo(400, takeProfitPixels);
-            ctx.stroke();
+            ctxCanvas.strokeStyle = "green";
+            ctxCanvas.lineWidth = 3;
+            ctxCanvas.beginPath();
+            ctxCanvas.moveTo(0, takeProfitPixels);
+            ctxCanvas.lineTo(400, takeProfitPixels);
+            ctxCanvas.stroke();
 
-            ctx.strokeStyle = "black";
+            ctxCanvas.strokeStyle = "black";
 
 
 
-            profit = trade(stockArrayCoords, isLong, amount, stopLoss, takeProfit, priceCanvas1);
+            profit = trade(stockArrayCoords, isLong, amount, stopLoss, takeProfit, priceCanvas);
             userCredits = userCredits + profit;
+;
+            drawStock(stockArrayDraw1, false, ctxCanvas1);
+            drawStock(stockArrayDraw2, false, ctxCanvas2); 
+            drawStock(stockArrayDraw3, false, ctxCanvas3); 
+            drawStock(stockArrayDraw4, false, ctxCanvas4); 
 
-            drawStock(stockArray1draw, false);
-            priceCanvas1 = calculateNewPrice(stockArrayCoords, priceCanvas1);
+            priceCanvas1 = calculateNewPrice(stockArrayCoords1, priceCanvas1);
+            priceCanvas2 = calculateNewPrice(stockArrayCoords2, priceCanvas2);
+            priceCanvas3 = calculateNewPrice(stockArrayCoords3, priceCanvas3);
+            priceCanvas4 = calculateNewPrice(stockArrayCoords4, priceCanvas4);
+
             console.log("user credits: " + userCredits);
             console.log("canvas price: " + priceCanvas1);
             console.log("==================================");
@@ -306,7 +374,7 @@ function drawThe500lines(stockArray, price) {
 }
 
 // Draws the stock price on a canvas
-function drawStock(coordinatesArray, isHalf) {
+function drawStock(coordinatesArray, isHalf, ctx) {
     let startingIndex;
     let startingPixel;
     let endingIndex;
